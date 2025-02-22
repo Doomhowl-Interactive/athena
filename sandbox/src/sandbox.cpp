e0f150b2e161e779b0b60d28e8ed81d17dde471a
bramtechs <bramtech@telenet.be>
2025-02-23 00:26:30 +0100

    wip

--- sandbox/src/sandbox.cpp
+++ sandbox/src/sandbox.cpp
@@ -2,65 +2,54 @@
 #include <athena.h>
 #include <iostream>
 #include <raylib.h>
+#include <vector>
 
-// represents old version
-struct header
+struct DoomPackageHeader
 {
-    const char test[13] = "helping hand";
+    const char magic[13] = "DOOM";
+    const uint32_t version = 1;
+    const uint32_t assetCount;
+
+    DoomPackageHeader() = default;
+
+    DoomPackageHeader(int count) : assetCount(count)
+    {
+    }
 };
 
-// represents new version
-struct header2
+struct DoomAssetHeader
 {
-    const char test[13] = "helping hand";
-    size_t size = 250;
-};
-
-struct header3
-{
-    const char test[15] = "sjsefsfessesef";
-    size_t size = 250;
-    uint32_t subsize = 651516;
-};
-
-struct backHeader
-{
-    const char head[5] = "LPRJ";
+    char fileName[128];
+    uint64_t size;
 };
 
 int main()
 {
-    std::vector<header> headers;
-    headers.resize(100);
+    std::filesystem::current_path(std::filesystem::path(__FILE__).parent_path().parent_path());
 
-    std::vector<header2> headers2;
-    headers2.resize(100);
+    try
+    {
+        auto path = std::filesystem::path("./assets.doom");
+        auto streamWriter = athena::fileStreamWriter(path);
+        DoomPackageHeader pkgHeader{1};
+        streamWriter.writeObject(pkgHeader);
+        streamWriter.nextStreamSection();
 
-    std::vector<header3> headers3;
-    headers3.resize(100);
-    std::cout << "Current working directory: " << std::filesystem::current_path() << std::endl;
+        {
+            int fileSize;
+            unsigned char *data = LoadFileData("ComfyUI_00372_.png", &fileSize);
 
-    std::filesystem::path path = "./temp.bin";
+            auto header = DoomAssetHeader{"ComfyUI_00372_.png", static_cast<uint64_t>(fileSize)};
+            streamWriter.writeObject(header);
+            streamWriter.writeData((const char *)data, fileSize);
+        }
 
-    athena::fileStreamWriter *streamWriter = new athena::fileStreamWriter(path);
-    streamWriter->writeArray(headers);
-    streamWriter->writeArray(headers3);
-    streamWriter->nextStreamSection();
-    streamWriter->writeArray(headers2);
-    streamWriter->writeArray(headers3);
-    streamWriter->flush();
-
-    delete streamWriter;
-
-    athena::fileStreamReader *streamReader = new athena::fileStreamReader(path);
-
-    std::vector<header> headersRead = streamReader->readArray<header>();
-    std::vector<header3> headersRead3 = streamReader->readArray<header3>();
-
-    streamReader->nextStreamSection();
-
-    std::vector<header2> headersRead2 = streamReader->readArray<header2>();
-    std::vector<header3> headersRead3_2 = streamReader->readArray<header3>();
+        streamWriter.flush();
+    }
+    catch (const std::exception& ex)
+    {
+        std::cerr << ex.what() << std::endl;
+    }
 
     InitWindow(800, 600, "raylib athena asset serialization");
     while (!WindowShouldClose())
