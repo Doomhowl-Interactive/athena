a9e7e3ec0dc8bf0226eccbc89684458005918cfd
bramtechs <bramtech@telenet.be>
2025-02-23 23:49:06 +0100

    let LZ4F choose the buffer size: fixes too small buffer error on larger inputs

--- Athena/src/streams/compression/compressedBuffer.cpp
+++ Athena/src/streams/compression/compressedBuffer.cpp
@@ -6,14 +6,14 @@ namespace athena
 {
 	compressedBuffer::compressedBuffer(buffer buffer)
 	{
-		m_data.resize(buffer.size() + sizeof(LZ4F_frameInfo_t));
-
 		LZ4F_preferences_t preferences{};
 		preferences.compressionLevel = LZ4F_compressionLevel_max();
 		preferences.frameInfo = LZ4F_INIT_FRAMEINFO;
 		preferences.frameInfo.contentSize = buffer.size();
 		preferences.frameInfo.contentChecksumFlag = LZ4F_contentChecksumEnabled;
 
+		m_data.resize(LZ4F_compressBound(buffer.size(), &preferences));
+		
 		size_t size = LZ4F_compressFrame(m_data.data(), m_data.size(), buffer.data(), buffer.size(), &preferences);
 
 		if (LZ4F_isError(size))
